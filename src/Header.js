444b1afec09707898b206466b50fef2606f10f69
Juan Manuel Alvarez <43937673+jm-alvarezs@users.noreply.github.com>
2020-05-10 07:59:16 -0500

    Feat/hero (#10)
    * added background image to hero
    
    * added icons to header

--- src/Header.js
+++ src/Header.js
@@ -8,11 +8,11 @@ export default function () {
         <img alt="Logo de Emergencia con Transparencia" height="auto" src="img/ect_logo.png" width="350px"></img>
       </div>
       <nav className="social">
-        <a href="https://www.facebook.com/RedesQuintoPoder/">Facebook</a>
-        <a href="https://twitter.com/rqpidea">Twitter</a>
-        <a href="https://www.youtube.com/user/RedesQuintoPoder">Youtube</a>
-        <a href="https://www.instagram.com/redesquintopoder/">Instagram</a>
-        <a href="https://redesquintopoder.org.mx/">Sitio</a>
+        <a href="https://www.facebook.com/RedesQuintoPoder/"><i className="fa fa-facebook"></i></a>
+        <a href="https://twitter.com/rqpidea"><i className="fa fa-twitter"></i></a>
+        <a href="https://www.youtube.com/user/RedesQuintoPoder"><i className="fa fa-youtube"></i></a>
+        <a href="https://www.instagram.com/redesquintopoder/"><i className="fa fa-instagram"></i></a>
+        <a href="https://redesquintopoder.org.mx/"><i className="fa fa-wesbite"></i></a>
       </nav>
     </header>
   );
