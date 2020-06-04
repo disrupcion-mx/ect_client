1705f0dddbe94c97e0899ea292c328ba92e2aed9
Juan Manuel Alvarez <43937673+jm-alvarezs@users.noreply.github.com>
2020-06-04 17:42:01 -0500

    fixed error on descargables

--- src/Descargables.js
+++ src/Descargables.js
@@ -4,8 +4,8 @@ export default function() {
     return (
         <div className="main">
             <h1>Descargables</h1>            
-            <p><a href="/tabla/texto">DOCUMENTO PROPUESTA DE TABLA DE GASTO- TEXTO</a></p>
-            <p><a href="/tabla/excell">DOCUMENTO PROPUESTA DE TABLA DE GASTO- TEXTO</a></p>
+            <p><a href="/tabla/texto">DOCUMENTO PROPUESTA DE TABLA DE GASTO - TEXTO</a></p>
+            <p><a href="/tabla/excell">DOCUMENTO PROPUESTA DE TABLA DE GASTO - EXCEL</a></p>
         </div>
     );
 }
